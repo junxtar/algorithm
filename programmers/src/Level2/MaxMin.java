7e00da83cff3a11229f4e1248c400518c9658c7e
junxtar <aa1870088@naver.com>
2023-01-18 18:50:57 +0900

    MaxMin

--- programmers/src/Level2/MaxMin.java
+++ programmers/src/Level2/MaxMin.java
@@ -7,13 +7,13 @@ public class MaxMin {
 	
 	// s에 들어있는 문자열 요소를 담을 정수형 배열을 선언 
 	static int[] arr;
-
 	public String solution(String s) {
 		split(s);
 		Arrays.sort(arr);
 		return arr[0] + " " + arr[arr.length - 1];
 	}
-
+	
+	
 	//StringTokenizer를 이용하여 분리한 후 배열에 저장 
 	public void split(String s) {
 		StringTokenizer st = new StringTokenizer(s);
