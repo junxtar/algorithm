ff7f2c9f04a06991f0c893539b330be8236880c7
junxtar <aa1870088@naver.com>
2023-02-04 15:05:11 +0900

    거스름돈

---
+++ programmers/src/Level3/ChangeMoney.java
@@ -0,0 +1,15 @@
+package Level3;
+
+public class ChangeMoney {
+
+	public int solution(int n, int[] money) {
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		for (int i = 0; i < money.length; i++) {
+			for (int j = money[i]; j <= n; j++) {
+				dp[j] += (dp[j - money[i]] % 1000000007);
+			}
+		}
+		return dp[n] % 1000000007;
+	}
+}
