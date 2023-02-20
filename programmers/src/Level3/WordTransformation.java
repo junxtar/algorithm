02bbb6d9e6c262bb21a7a6d6e84ecd7a4b057119
junxtar <aa1870088@naver.com>
2023-02-20 12:40:17 +0900

    단어 변환

---
+++ programmers/src/Level3/WordTransformation.java
@@ -0,0 +1,52 @@
+package Level3;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+//단어 변환 
+public class WordTransformation {
+	static class Node {
+		String str;
+		int z;
+
+		Node(String str, int z) {
+			this.str = str;
+			this.z = z;
+		}
+	}
+
+	static boolean[] visited;
+
+	public int solution(String begin, String target, String[] words) {
+		visited = new boolean[words.length];
+		return dfs(begin, target, words);
+
+	}
+
+	static int dfs(String begin, String target, String[] words) {
+		Queue<Node> q = new LinkedList<>();
+		q.offer(new Node(begin, 0));
+
+		while (!q.isEmpty()) {
+			Node cur = q.poll();
+			if (cur.str.equals(target)) {
+				return cur.z;
+			}
+			for (int i = 0; i < words.length; i++) {
+				int count = 0;
+				if (!visited[i]) {
+					for (int j = 0; j < words[i].length(); j++) {
+						if (cur.str.charAt(j) == words[i].charAt(j)) {
+							count++;
+						}
+					}
+					if (count == words[i].length() - 1) {
+						q.offer(new Node(words[i], cur.z + 1));
+						visited[i] = true;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+}
