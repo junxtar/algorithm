268f536a9ef4f556b8ec220bb51f644136acc900
junxtar <aa1870088@naver.com>
2023-03-04 14:45:51 +0900

    할인 행사

--- programmers/src/Level2/DiscountEvent.java
+++ programmers/src/Level2/DiscountEvent.java
@@ -7,6 +7,7 @@ public class DiscountEvent {
 	public int solution(String[] want, int[] number, String[] discount) {
 		int answer = 0;
 		HashMap<String, Integer> map = new HashMap<>();
+		
 		for (int i = 0; i < 10; i++) {
 			map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
 		}
@@ -16,6 +17,7 @@ public class DiscountEvent {
 		for (int i = 10; i < discount.length; i++) {
 			map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
 			map.put(discount[i - 10], map.get(discount[i - 10]) - 1);
+			
 			if (isContains(map, want, number))
 				answer++;
 		}
