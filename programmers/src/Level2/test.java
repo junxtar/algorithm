63aa7c17153df8b84ba4b8a93b3402320e28a6f1
junxtar <aa1870088@naver.com>
2023-03-27 18:15:33 +0900

    test_file

--- programmers/src/Level2/test.java
+++ programmers/src/Level2/test.java
@@ -1,19 +1,11 @@
 package Level2;
 
-import java.util.HashMap;
-
 public class test {
 
 	public static void main(String[] args) {
-		HashMap<String, Integer>map = new HashMap<>();
-		map.put("aa", 3);
-		map.put("aa", map.get("aa") - 1);
-		
-		System.out.print(map.get("aa"));
+		String st = "ana";
+		System.out.println(st.indexOf('a'));
 		
 	}
-	//ab ba ab
-	//ba ab ba
-	//ab ba 2
-	//4 
 }
+
