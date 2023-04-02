ae546ea9f3f5f0c11f7677f3608b8dcc8e5592df
junxtar <aa1870088@naver.com>
2023-04-02 22:07:26 +0900

    테스트 파일

--- programmers/src/Level2/test.java
+++ programmers/src/Level2/test.java
@@ -1,10 +1,18 @@
 package Level2;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class test {
 
 	public static void main(String[] args) {
-		String st = "ana";
-		System.out.println(st.indexOf('a'));
+		Deque<Integer>q = new LinkedList<>();
+		q.offer(1);
+		q.offer(2);
+		q.offer(3);
+		q.offerFirst(5);
+		System.out.println(q.poll());
+		
 		
 	}
 }
