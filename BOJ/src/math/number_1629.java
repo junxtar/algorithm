a2f7379daa628c4351774607a94c4833af31198a
junxtar <aa1870088@naver.com>
2023-04-19 13:20:42 +0900

    곱셈

---
+++ BOJ/src/math/number_1629.java
@@ -0,0 +1,35 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class number_1629 {
+	
+	public static long C;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		C = Long.parseLong(st.nextToken());
+		
+		System.out.println(pow(A,B));
+
+	}
+	public static long pow(long A, long exponent) {
+		if (exponent == 1) {
+			return A % C;
+		}
+		long temp = pow(A, exponent / 2);
+		
+		if (exponent % 2 == 0) {
+			return temp * temp % C;
+		}
+		return (temp * temp % C) * A % C;
+	}
+
+}
