6f8b9fe4694364bf2b96b0403b250ef6dc427fe2
junxtar <aa1870088@naver.com>
2023-04-29 15:27:03 +0900

    N과M(9)

--- BOJ/src/Backtrankking/number_15663.java
+++ BOJ/src/Backtrankking/number_15663.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.StringTokenizer;
 
 public class number_15663 {
@@ -14,7 +14,7 @@ public class number_15663 {
 	static int[] arr;
 	static boolean[] visited;
 	static StringBuilder sb = new StringBuilder();
-	static HashSet<String> set = new HashSet<>();
+	static LinkedHashSet<String> set = new LinkedHashSet<>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
