59cbad0f84790f83a44a527f9d8ea031cf1a9d16
junxtar <aa1870088@naver.com>
2023-04-30 11:44:59 +0900

    N과M(12)

--- BOJ/src/Backtrankking/number_15666.java
+++ BOJ/src/Backtrankking/number_15666.java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class number_15666 {
 
