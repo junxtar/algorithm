4326507c2c573d47ce5173ca37dafaf66ce9f22b
junxtar <aa1870088@naver.com>
2023-05-10 13:35:37 +0900

    가장 긴 증가하는 부분 수열 4

---
+++ BOJ/src/DP/number_14002.java
@@ -0,0 +1,24 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class number_14002 {
+	
+	static int N;
+	static int[] seq;
+	static int[] dp;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		seq = new int[N];
+		dp = new int[N];
+		st = new StringTokenizer(br.readLine());
+		
+	}
+
+}
