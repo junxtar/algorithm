49b1f17e02eaef9d11c99b661c6e6ab3eda02dd5
junxtar <aa1870088@naver.com>
2023-10-09 15:26:01 +0900

    음양 더하기

---
+++ programmers/src/level1/IntegerPlus.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class IntegerPlus {
+	public int solution(int[] absolutes, boolean[] signs) {
+		int answer = 0;
+		for (int i = 0; i < absolutes.length; i++) {
+			int value = absolutes[i];
+			answer += signs[i] ? value : -1 * value;
+
+		}
+		return answer;
+	}
+}
