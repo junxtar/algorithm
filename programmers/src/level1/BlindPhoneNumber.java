042a8bd8dc76db170dab789f5d8a5505c7155d1f
junxtar <aa1870088@naver.com>
2023-10-11 14:05:25 +0900

    휴대폰 번호 가리기

---
+++ programmers/src/level1/BlindPhoneNumber.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class BlindPhoneNumber {
+
+	public String solution(String phone_number) {
+		char[] ch = phone_number.toCharArray();
+		for (int i = 0; i < ch.length - 4; i++) {
+			ch[i] = '*';
+		}
+		return String.valueOf(ch);
+	}
+
+}
