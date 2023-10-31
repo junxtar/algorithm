5e460ff0c3aa441104bf5b9af68a4c13f154c4f4
junxtar <aa1870088@naver.com>
2023-10-31 18:55:57 +0900

    공원 산책

--- programmers/src/level1/ParkWalk.java
+++ programmers/src/level1/ParkWalk.java
@@ -3,63 +3,70 @@ package level1;
 import java.util.StringTokenizer;
 
 public class ParkWalk {
-	static int[] xy = { -1, 0, 1, 0, -1 };
+	private static final int[] MOVES = { -1, 0, 1, 0, -1 };
 
 	public int[] solution(String[] park, String[] routes) {
 		char[][] map = new char[park.length][park[0].length()];
+
+		int[] startingPosition = findStartingPosition(park, map);
+
+		for (String route : routes) {
+			startingPosition = moveMap(startingPosition, route, map);
+		}
+		return startingPosition;
+	}
+
+	private int[] findStartingPosition(String[] park, char[][] map) {
+		int[] startingPosition = new int[2];
 		for (int i = 0; i < park.length; i++) {
 			map[i] = park[i].toCharArray();
-		}
-		int row = 0;
-		int col = 0;
-		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[i].length; j++) {
 				if (map[i][j] == 'S') {
-					row = i;
-					col = j;
+					startingPosition[0] = i;
+					startingPosition[1] = j;
 				}
 			}
 		}
-
-		for (int i = 0; i < routes.length; i++) {
-			int[] dirArray = new int[2];
-			dirArray = moveMap(row, col, routes[i], map);
-			row = dirArray[0];
-			col = dirArray[1];
-
-		}
-		return new int[] { row, col };
+		return startingPosition;
 	}
 
-	public static int[] moveMap(int row, int col, String route, char[][] map) {
+	public static int[] moveMap(int[] currentPosition, String route, char[][] map) {
 		StringTokenizer st = new StringTokenizer(route);
 		int dir = dirNumber(st.nextToken());
 		int dis = Integer.parseInt(st.nextToken());
-		int nx = row;
-		int ny = col;
+		int nx = currentPosition[0];
+		int ny = currentPosition[1];
+
 		for (int i = 0; i < dis; i++) {
-			nx += xy[dir];
-			ny += xy[dir + 1];
-			if (nx < 0 | ny < 0 | nx >= map.length | ny >= map[0].length) {
-				return new int[] { row, col };
+			nx += MOVES[dir];
+			ny += MOVES[dir + 1];
+			if (isOutOfIndex(nx, ny, map)) {
+				return currentPosition;
 			}
 			if (map[nx][ny] == 'X') {
-				return new int[] { row, col };
+				return currentPosition;
 			}
 		}
 		return new int[] { nx, ny };
 	}
 
-	public static int dirNumber(String dir) {
-		if (dir.equals("N")) {
+	private static boolean isOutOfIndex(int row, int col, char[][] map) {
+		if (row < 0 | col < 0 | row >= map.length | col >= map[0].length) {
+			return true;
+		}
+		return false;
+	}
+
+	private static int dirNumber(String dir) {
+		switch (dir) {
+		case "N":
 			return 0;
-		}
-		if (dir.equals("E")) {
+		case "E":
 			return 1;
-		}
-		if (dir.equals("S")) {
+		case "S":
 			return 2;
+		default:
+			return 3;
 		}
-		return 3;
 	}
 }
