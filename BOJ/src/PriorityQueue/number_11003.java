5d5f1512d1bb50a4336d76722451ae5848c79308
junxtar <aa1870088@naver.com>
2023-12-05 18:56:25 +0900

    최솟값 찾기

--- BOJ/src/PriorityQueue/number_11003.java
+++ BOJ/src/PriorityQueue/number_11003.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class number_11003 {
