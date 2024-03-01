ecd9c7762ce55511588d0b5a021f819196745eeb
junxtar <aa1870088@naver.com>
2024-03-01 13:07:36 +0900

    데이터 분석

--- programmers/src/level1/DataAnalysis.java
+++ programmers/src/level1/DataAnalysis.java
@@ -1,5 +1,40 @@
 package level1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class DataAnalysis {
 
+	public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+		int[][] answer;
+		ArrayList<int[]> list = new ArrayList<>();
+
+		for (int[] arr : data)
+			if (arr[setCategory(ext)] < val_ext)
+				list.add(arr);
+
+		Collections.sort(list, (o1, o2) -> {
+			return o1[setCategory(sort_by)] - o2[setCategory(sort_by)];
+		});
+
+		answer = new int[list.size()][];
+
+		for (int i = 0; i < list.size(); i++)
+			answer[i] = list.get(i);
+
+		return answer;
+	}
+
+	public int setCategory(String category) {
+		if (category.equals("code")) {
+			return 0;
+		}
+		if (category.equals("date")) {
+			return 1;
+		}
+		if (category.equals("maximum")) {
+			return 2;
+		}
+		return 3;
+	}
 }
