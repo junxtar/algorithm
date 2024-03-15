e98e689dc8b7f0483cbb4ab6110830e4c984248f
junxtar <aa1870088@naver.com>
2024-03-15 13:11:27 +0900

    인사고과

--- programmers/src/Level3/PerformanceReview.java
+++ programmers/src/Level3/PerformanceReview.java
@@ -1,28 +1,14 @@
 package Level3;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class PerformanceReview {
-
-	static class Person {
-		int index;
-		int sum;
-
-		Person(int index, int sum) {
-			this.index = index;
-			this.sum = sum;
-		}
-	}
-
-	static List<Person> list = new ArrayList<>();
 
 	public int solution(int[][] scores) {
 		int n = scores[0][0];
 		int m = scores[0][1];
 		int score = n + m;
+
 		Arrays.sort(scores, (o1, o2) -> {
 			if (o1[0] == o2[0]) {
 				return o1[1] - o2[1];
@@ -32,31 +18,28 @@ public class PerformanceReview {
 
 		int flag = scores[0][1];
 		for (int i = 0; i < scores.length; i++) {
-			int sum = scores[i][0] + scores[i][1];
 			if (flag <= scores[i][1]) {
 				flag = scores[i][1];
-				list.add(new Person(i, sum));
 				continue;
 			}
 			if (n == scores[i][0] && m == scores[i][1]) {
 				return -1;
 			}
+			scores[i][0] = -1;
+			scores[i][1] = -1;
 		}
 
-		Collections.sort(list, (o1, o2) -> o2.sum - o1.sum);
+		Arrays.sort(scores, (o1, o2) -> (o2[0] + o2[1]) - (o1[0] + o1[1]));
 
-		int left = 0;
-		int right = list.size();
+		int answer = 1;
 
-		while (left < right) {
-			int mid = (left + right) / 2;
-			if (list.get(mid).sum <= score) {
-				right = mid;
-			} else {
-				left = mid + 1;
+		for (int i = 0; i < scores.length; i++) {
+			if (scores[i][0] + scores[i][1] > score) {
+				answer++;
+				continue;
 			}
+			break;
 		}
-
-		return left + 1;
+		return answer;
 	}
 }
